3185e1b2ce112e5360c7d279fea77ad52c2dc8f2
Fatlum Mehmeti <126803678+FatlumMehmeti@users.noreply.github.com>
2026-05-23 02:02:50 +0200

    ci: add Cloud Run deploy workflow and .dockerignore

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
-import { app } from './app.js';
-import { env } from './config/env.js';
-import { attachRealtime } from './realtime.js';
+import { app } from "./app.js";
+import { env } from "./config/env.js";
+import { attachRealtime } from "./realtime.js";
 
-const server = app.listen(env.port, () => {
-  console.log(`SAFE backend running on http://localhost:${env.port}`);
+const server = app.listen(env.port, "0.0.0.0", () => {
+  console.log(`SAFE backend running on http://0.0.0.0:${env.port}`);
 });
 
 attachRealtime(server, app);
